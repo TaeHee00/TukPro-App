874e0f6694691f8bbfaefe37441db3b27cb09128
TaeHee00 <kancth03@gmail.com>
2023-10-16 10:03:36 +0900

    hotfix: login 비즈니스 로직의 관심사 분리를 위해 exception 코드 분리

---
+++ lib/exception/login_exception.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+
+class LoginException {
+  LoginException(this.messageSender);
+  final void Function(String message) messageSender;
+
+  bool test(Response response) {
+    if (response.statusCode! / 100 == 4 || response.statusCode! / 100 == 5 ||
+        response.data == null || response.data == "null") {
+      // server error
+      return false;
+    }
+
+    final String message = response.data!["message"];
+    if (message == "EMAIL_MISMATCH") {
+      // 존재하지 않는 계정
+      messageSender("존재하지 않는 계정입니다.");
+    }
+    if (message == "PASSWORD_MISMATCH") {
+      // 올바르지 않은 비밀번호
+      messageSender("올바르지 않은 비밀번호입니다.");
+    }
+    return true;
+  }
+}
