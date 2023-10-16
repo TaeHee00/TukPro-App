55ef6123f9104436ab56d1db6ba68aafc40d4530
SNOW <kancth03@gmail.com>
2023-10-17 03:54:24 +0900

    fix: Material3를 적용했을 경우 ListView Widget이 AppBar 백그라운드 컬러에 간섭이 생겨 제거

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,35 @@
 import 'package:flutter/material.dart';
+<<<<<<< Updated upstream
 import 'package:tuk_pro_app/screens/login.dart';
+=======
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tuk_pro_app/provider/login_session_provider.dart';
+import 'package:tuk_pro_app/views/login.dart';
+import 'package:tuk_pro_app/views/project_list.dart';
+>>>>>>> Stashed changes
 
 void main() {
-  runApp(const App());
+  runApp(ProviderScope(child: App()));
 }
 
-class App extends StatelessWidget {
+class App extends ConsumerWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final hasLoginSession = ref.watch(loginSessionProvider).isLogin;
+
     return MaterialApp(
       title: "TukPro",
       theme: ThemeData().copyWith(
-        useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(
             seedColor: const Color.fromARGB(255, 232, 243, 252)),
+<<<<<<< Updated upstream
+=======
+        // textTheme: Theme.of(context).textTheme.apply(fontFamily: GoogleFonts.openSans())
+>>>>>>> Stashed changes
       ),
-      home: const LoginScreen(),
+      home: hasLoginSession ? ProjectListScreen() : const LoginScreen(),
     );
   }
 }
