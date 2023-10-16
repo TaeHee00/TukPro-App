667d2f68aa99ca787f1a93554391e5d4ada1bcbb
SNOW <kancth03@gmail.com>
2023-10-17 07:47:57 +0900

    fix: 진행프로젝트 관련 코드 제거 후 위젯으로 변경

--- lib/views/project_list.dart
+++ lib/views/project_list.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:tuk_pro_app/views/main_header.dart';
+import 'package:tuk_pro_app/views/widgets/main_header.dart';
+import 'package:tuk_pro_app/views/widgets/progressing_project.dart';
 
 class ProjectListScreen extends ConsumerStatefulWidget {
   const ProjectListScreen({super.key});
@@ -38,108 +39,8 @@ class _ProjectListScreenState extends ConsumerState<ProjectListScreen> {
                     child: Column(
                       children: [
                         // const SizedBox(height: 2),
-                        Padding(
-                          padding: const EdgeInsets.all(20),
-                          child: Card(
-                            elevation: 5,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(18)),
-                            color: Colors.white,
-                            child: Container(
-                              width: double.infinity,
-                              child: Padding(
-                                padding: const EdgeInsets.all(16.0),
-                                child: Column(
-                                  children: [
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Text(
-                                          "진행중인 프로젝트",
-                                          style: GoogleFonts.nanumGothic(
-                                            fontWeight: FontWeight.w800,
-                                            fontSize: 20,
-                                          ),
-                                        ),
-                                        const Spacer(),
-                                        const Icon(
-                                          Icons.chevron_right_outlined,
-                                          size: 30,
-                                        )
-                                      ],
-                                    ),
-                                    // const SizedBox(
-                                    //   height: 5,
-                                    // ),
-                                    Divider(
-                                      color: Colors.grey,
-                                    ),
-                                    const SizedBox(height: 5,),
-                                    Row(
-                                      children: [
-                                        ClipRRect(
-                                          borderRadius: BorderRadius.circular(12.0),
-                                          child: Image.asset(
-                                            "assets/images/dummy_project_img.png",
-                                            fit: BoxFit.cover,
-                                            width: 90,
-                                            height: 100,
-                                          ),
-                                        ),
-                                        const SizedBox(width: 10,),
-                                        Expanded(
-                                          child: Column(
-                                            crossAxisAlignment: CrossAxisAlignment.start,
-                                            children: [
-                                              const SizedBox(height: 6,),
-                                              Text(
-                                                "대규모 트래픽 처리 소셜 서비스",
-                                                style: GoogleFonts.nanumGothic(
-                                                  fontWeight: FontWeight.bold,
-                                                  fontSize: 18,
-                                                ),
-                                                softWrap: true,
-                                              ),
-                                              const SizedBox(height: 15,),
-                                              Text("Task: 로그인 서버 구현",
-                                                style: GoogleFonts.nanumGothic(
-                                                  fontWeight: FontWeight.bold,
-                                                  fontSize: 14,
-                                                  color: Colors.black54
-                                                ),
-                                                softWrap: true,
-                                              ),
-                                              const SizedBox(height: 5,),
-                                              Text("Task Deadline: 10-21 12:30",
-                                                style: GoogleFonts.nanumGothic(
-                                                    fontWeight: FontWeight.bold,
-                                                    fontSize: 14,
-                                                    color: Colors.black54
-                                                ),
-                                                softWrap: true,
-                                              ),
-                                              const SizedBox(height: 5,),
-                                              Text("다음 회의: 10-21 21:30",
-                                                style: GoogleFonts.nanumGothic(
-                                                    fontWeight: FontWeight.bold,
-                                                    fontSize: 14,
-                                                    color: Colors.black54
-                                                ),
-                                                softWrap: true,
-                                              ),
-                                              const SizedBox(height: 10,)
-                                            ],
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
+                        ProgressingProject(),
+                        ProgressingProject(),
                       ],
                     ),
                   ),
